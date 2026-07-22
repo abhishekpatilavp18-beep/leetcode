75ad52d6bd69462ca993d151a4129359936f8b3a
abhishekpatilavp18-beep <abhishekpatilavp18@gmail.com>
2026-07-22 19:50:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.28%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int i;
+        int ans;
+        for(i=0;i<=30;i++){
+            ans = pow(2,i);
+            if(n==ans){
+                return 1;
+
+            } 
+        }return 0;
+    }
+};
